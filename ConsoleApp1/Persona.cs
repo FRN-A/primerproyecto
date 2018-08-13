b7493ace4c46bae6eb38b6ffa4775013c6f343ba
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-13 08:17:03 -0700

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Persona
+    {
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+
+        public Persona(int edad, string nombre)
+        {
+            this.Edad = edad;
+            this.Nombre = nombre;
+        }
+
+        public Persona()
+        {
+            this.Edad = 0;
+            this.Nombre = "";
+        }
+
+        public void cumplirAno()
+        {
+            this.Edad++;
+        }
+    }
+}
